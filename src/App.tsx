5725a12795b3cd8c33d1e20904e4d052a0863669
Raboro <marius.woerfelreal@gmail.com>
2024-09-29 12:31:13 +0200

    refactor(Biome): :art: used biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Heading } from './components/Heading/Heading.tsx';
-import { TextInput } from './components/TextInput/TextInput.tsx'
+import { TextInput } from './components/TextInput/TextInput.tsx';
 
 function App() {
-  return <div className='mainContainer'>
-    <Heading />
-    <TextInput />
-  </div>;
+  return (
+    <div className="mainContainer">
+      <Heading />
+      <TextInput />
+    </div>
+  );
 }
 
 export default App;
