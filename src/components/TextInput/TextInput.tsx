8e039cca143618f1440a26b0637cc0c511df0960
Raboro <marius.woerfelreal@gmail.com>
2024-09-29 12:42:48 +0200

    feat(Styling): :lipstick: added styling

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,9 +1,11 @@
+import './TextInput.css'
+
 export function TextInput() {
   return (
-    <div>
-      <label>
+    <div className='textareaContainer'>
+      <label className='label'>
         Text Input
-        <textarea name="TextInput" defaultValue="Reading Text" />
+        <textarea className='textarea' name="TextInput" defaultValue="Reading Text" />
       </label>
     </div>
   );
