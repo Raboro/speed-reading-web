ccf816306c0fbbb7a785f386f6e615af29d8788b
Raboro <marius.woerfelreal@gmail.com>
2024-09-29 20:40:10 +0200

    feat(App): :sparkles: changed word after 1s

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Heading } from './components/Heading/Heading.tsx';
 import { TextInput } from './components/TextInput/TextInput.tsx';
@@ -7,15 +7,27 @@ import { Word } from './components/Word/Word.tsx';
 
 function App() {
   const [text, setText] = useState<Text>(new Text(''));
+  const [word, setWord] = useState<string>('');
+
   const collectText = (collectedText: string) => {
     setText(new Text(collectedText));
   };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setWord(text.get());
+      console.debug(word);
+    }, 1000); // 1s
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [text, word]);
 
   return (
     <div className="mainContainer">
       <Heading />
       <TextInput collectText={collectText} />
-      <Word word="test" />
+      <Word word={word} />
     </div>
   );
 }
