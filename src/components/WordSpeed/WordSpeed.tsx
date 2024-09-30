70e32d6e2e2c5dc4c0b7b5eee2dbcac39a061099
Raboro <marius.woerfelreal@gmail.com>
2024-09-30 09:33:59 +0200

    feat(WordSpeed): :sparkles: added WordSpeed component

---
+++ src/components/WordSpeed/WordSpeed.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './WordSpeed.css';
+
+interface WordSpeedProps {
+  onWpsChange: (wps: number) => void;
+}
+
+export function WordSpeed(props: Readonly<WordSpeedProps>) {
+  const [wps, setWps] = useState<number>(60);
+
+  const calculateWPS = (value: number) => {
+    setWps(value);
+    if (value === 0) {
+      props.onWpsChange(0);
+      return;
+    }
+    props.onWpsChange(value / 60);
+  };
+
+  return (
+    <div className="wordSpeedContainer">
+      <input
+        type="range"
+        list="markers"
+        max={600}
+        defaultValue={60}
+        onChange={(event) =>
+          calculateWPS(event.target.value as unknown as number)
+        }
+      />
+      <datalist id="markers">
+        <option value="0">0</option>
+        <option value="30">30</option>
+        <option value="60">60</option>
+        <option value="100">100</option>
+        <option value="150">150</option>
+        <option value="200">200</option>
+        <option value="300">300</option>
+        <option value="400">400</option>
+        <option value="500">500</option>
+        <option value="600">600</option>
+      </datalist>
+      <h3>Words per minute: {wps}</h3>
+    </div>
+  );
+}
